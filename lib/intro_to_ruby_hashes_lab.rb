542e4bdbe61ef55e62e08da61bc783c7c30a2401
Alex S Grimes <alex@alexgrimes.com>
2020-04-15 00:11:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,5 @@
 def my_hash_creator(key, value)
-  { name: value }
+  { key => value }
 end
 
 def read_from_hash(hash, key)
@@ -15,8 +15,6 @@ def update_counting_hash(hash, key)
 end 
 
 def update_counting_hash(hash, key)
-  hash[key] ?
-  hash[key] += 1 :
-  hash[key] = 1 
+  hash[key] ? hash[key] += 1 : hash[key] = 1 
     hash 
 end 
